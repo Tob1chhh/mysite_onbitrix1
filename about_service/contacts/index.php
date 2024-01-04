d3cf1ebcbf29aeac4f3e8c03794843769de4e724
Ivan Muzalev <muzaloff_ivan@mail.ru>
2024-01-04 13:26:38 +0300

    Task 8: Implementation of the feedback form

--- about_service/contacts/index.php
+++ about_service/contacts/index.php
@@ -1,13 +1,48 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
-?><br><h1>Контактная информация.</h1><br>
-<?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_SHOW" => "page",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => ""
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<div class="site-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-lg-8 mb-5">
+			 <?$APPLICATION->IncludeComponent(
+				"bitrix:main.feedback", 
+				"feedback_form", 
+				array(
+					"COMPONENT_TEMPLATE" => "feedback_form",
+					"EMAIL_TO" => "info@domain.com",
+					"EVENT_MESSAGE_ID" => array(
+						0 => "7",
+					),
+					"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+					"REQUIRED_FIELDS" => array(
+						0 => "NAME",
+						1 => "EMAIL",
+						2 => "SUBJECT",
+						3 => "MESSAGE",
+					),
+					"USE_CAPTCHA" => "Y"
+				),
+				false
+			);?>
+			</div>
+			<div class="col-lg-4">
+				<div class="p-4 mb-3 bg-white">
+					<?$APPLICATION->IncludeComponent(
+					"bitrix:main.include", 
+					".default", 
+					array(
+						"AREA_FILE_SHOW" => "page",
+						"AREA_FILE_SUFFIX" => "inc",
+						"EDIT_TEMPLATE" => "",
+						"COMPONENT_TEMPLATE" => ".default"
+					),
+					false
+				);?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- about_service/contacts/index_inc.php
+++ about_service/contacts/index_inc.php
@@ -1,4 +1,11 @@
 <div>
-    <br><p>Наш контактный телефон: +2 102 3923 3922</p>
-    <p>Наш контактный адрес электронной почты: info@domain.com</p>
+	<h3 class="h6 text-black mb-3 text-uppercase">Contact Info</h3>
+	<p class="mb-0 font-weight-bold">Address</p>
+	<p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
+
+	<p class="mb-0 font-weight-bold">Phone</p>
+	<p class="mb-4"><a href="#">+2 102 3923 3922</a></p>
+
+	<p class="mb-0 font-weight-bold">Email Address</p>
+	<p class="mb-0"><a href="#">info@domain.com</a></p>
 </div>
